f253c81634031923b1c676fc7b464d74a4d7b33a
saptarshi <sapbum1234@gmail.com>
2025-02-08 17:25:03 +0530

    CORS fix

--- src/main/java/com/medshop/Medicine/Controller/CustomerController.java
+++ src/main/java/com/medshop/Medicine/Controller/CustomerController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/customers")
+@CrossOrigin("*")
 public class CustomerController {
 
     @Autowired
--- src/main/java/com/medshop/Medicine/Controller/TestController.java
+++ src/main/java/com/medshop/Medicine/Controller/TestController.java
@@ -1,10 +1,12 @@
 package com.medshop.Medicine.Controller;
 
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin("*")
 public class TestController {
 
     @GetMapping("/test")
