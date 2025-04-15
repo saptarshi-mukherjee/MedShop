a8f3be503c02594c71f224cea89a8f9215a4375b
saptarshi <sapbum1234@gmail.com>
2025-04-15 10:52:36 +0530

    set-up

---
+++ src/main/java/com/MedShop/quick_com/QuickComApplication.java
@@ -0,0 +1,13 @@
+package com.MedShop.quick_com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickComApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickComApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MedShop/quick_com/QuickComApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MedShop.quick_com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickComApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
